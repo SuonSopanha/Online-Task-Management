c5eabf1bebc4183c3d9afb0b5e4a531ac4f3c9c3
Sopanha <hachwalthjugram@gmail.com>
2024-01-06 08:11:46 +0700

    private upload

---
+++ src/firebase/credentail.js
@@ -0,0 +1,11 @@
+const firebase_credentail = {
+FIREBASE_API_KEY:"AIzaSyAfbx3jwomD50DjAybXDuP5mXY3NYZ_2eU",
+FIREBASE_AUTH_DOMAIN:"online-task-management-15ae7.firebaseapp.com",
+FIREBASE_PROJECT_ID:"online-task-management-15ae7",
+FIREBASE_STORAGE_BUCKET:"online-task-management-15ae7.appspot.com",
+FIREBASE_MESSAGING_SENDER_ID:"400038243237",
+FIREBASE_APP_ID:"1:400038243237:web:4c69ba2d9266e5a41d964c",
+}
+
+export default firebase_credentail;
+
